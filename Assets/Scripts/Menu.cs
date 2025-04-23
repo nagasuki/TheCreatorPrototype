f21c634617eb8a41b630d15a8cdee40acaf710e8
Nagasuki <tiyanon.sa@gmail.com>
2025-04-23 18:19:12 +0700

    update

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,9 +9,11 @@ public class Menu : MonoBehaviour
     [SerializeField] private Button joinButton;
     [SerializeField] private TMP_InputField ipInputField;
     [SerializeField] private Button exitButton;
+    [SerializeField] private TMP_Dropdown dropdown;
 
     void Start()
     {
+        NetworkManager.singleton.onlineScene = dropdown.options[0].text;
         hostButton.onClick.AddListener(() => NetworkManager.singleton.StartHost());
         joinButton.onClick.AddListener(() => NetworkManager.singleton.StartClient());
         exitButton.onClick.AddListener(() => Application.Quit());
@@ -20,5 +22,10 @@ public class Menu : MonoBehaviour
     public void OnInputFieldChanged(string text)
     {
         NetworkManager.singleton.networkAddress = text;
+    }
+
+    public void OnDropdownValueChanged(int value)
+    {
+        NetworkManager.singleton.onlineScene = dropdown.options[value].text;
     }
 }
